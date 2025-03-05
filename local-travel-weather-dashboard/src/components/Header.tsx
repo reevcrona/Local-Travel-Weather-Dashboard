f9ba502074a3861be57be2e6486acb8ebc02f329
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-03-05 17:12:22 +0100

    still working on solving some buggs that the google api wrapper has

--- local-travel-weather-dashboard/src/components/Header.tsx
+++ local-travel-weather-dashboard/src/components/Header.tsx
@@ -1,22 +1,53 @@
-import Autocomplete from "react-google-autocomplete";
+import { usePlacesWidget } from "react-google-autocomplete";
+import { useRef } from "react";
 
 function Header() {
   const googleApiKey = import.meta.env.VITE_GOOGLE_API_KEY;
+  const hasPlaceSelected = useRef(false); // Create a ref to track if the function has been called
+  const previousPlace = useRef(null);
+  const { ref } = usePlacesWidget({
+    apiKey: googleApiKey,
+    onPlaceSelected: (place) => {
+      if (
+        !place ||
+        hasPlaceSelected.current ||
+        !place.geometry ||
+        previousPlace === place
+      ) {
+        if (ref.current) {
+          ref.current.value = "";
+        }
+        return;
+      } // Return early if the function has already been called
+      hasPlaceSelected.current = true; // Set the flag to true
+
+      if (place.geometry && place.geometry.location) {
+        const lat = place.geometry.location.lat();
+        const lng = place.geometry.location.lng();
+        console.log(place);
+        console.log(lat, lng);
+      }
+      previousPlace.current = place;
+      if (ref.current) {
+        ref.current.value = "";
+      }
+
+      // Reset the flag after a short delay to allow the function to be called again
+      setTimeout(() => {
+        hasPlaceSelected.current = false;
+      }, 200);
+    },
+    options: {
+      types: [],
+      componentRestrictions: { country: "swe" },
+    },
+  });
+
   return (
     <>
-      <Autocomplete
-        apiKey={googleApiKey}
-        style={{ width: "500px", backgroundColor: "black", color: "white" }}
-        onPlaceSelected={(place) => {
-          const lat = place.geometry.location.lat();
-          const lng = place.geometry.location.lng();
-          console.log(`Lat: ${lat}   Lng: ${lng}`);
-        }}
-        options={{
-          types: [],
-          componentRestrictions: { country: "swe" },
-        }}
-      />
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input ref={ref} type="text" style={{ width: "500px" }}></input>
+      </form>
     </>
   );
 }
