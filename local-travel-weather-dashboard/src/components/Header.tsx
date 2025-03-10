2e8e209d491ab6fc6a57ef108ee10b70f750354b
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-03-10 15:45:50 +0100

    added some base styling for header

--- local-travel-weather-dashboard/src/components/Header.tsx
+++ local-travel-weather-dashboard/src/components/Header.tsx
@@ -1,9 +1,18 @@
 import AutocompleteInput from "./AutocompleteInput";
-
+import mapLogo from "../assets/maps.svg";
 function Header() {
   return (
-    <header>
-      <AutocompleteInput />
+    <header className="grid grid-cols-3 items-center min-h-44 px-16">
+      <div className="col-span-1">
+        <img className="w-32 h-20 " src={mapLogo} alt="Map Logo"></img>
+      </div>
+
+      <div className="col-span-1 justify-self-center">
+        <h1 className="text-3xl text-center mb-7">
+          Local Travel & Weather Dashboard
+        </h1>
+        <AutocompleteInput />
+      </div>
     </header>
   );
 }
