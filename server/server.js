aae30f2f3fa2d7c048efa4cbb1b7d79c655fcd95
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-03-13 21:41:19 +0100

    added so we can get info about trafic situations from trafikverkets api and send that info to the frontend, that is a todo.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,41 @@
 import express from "express";
+import cors from "cors";
+import axios from "axios";
+import dotenv from "dotenv";
 const app = express();
+const port = 3000;
 const corsOptions = {
     origin: ["http://localhost:5173"],
 };
-const port = 3000;
+app.use(cors(corsOptions));
+dotenv.config();
+const TRAFIKVERKET_API_KEY = process.env.TRAFIKVERKET_API_KEY;
+const API_URL = "https://api.trafikinfo.trafikverket.se/v2/data.json";
+const xmlData = `
+<REQUEST>
+    <LOGIN authenticationkey="${TRAFIKVERKET_API_KEY}"/>
+    <QUERY objecttype="Situation" schemaversion="1.5" limit="10">
+        <FILTER>
+            <NEAR name="Deviation.Geometry.Point.WGS84" value="56.2445598 12.8583161" maxdistance="10000"/>
+        </FILTER>
+    </QUERY>
+</REQUEST>
+`;
 app.get("/", (req, res) => {
-    res.send("yo");
+    axios
+        .post(API_URL, xmlData, {
+        headers: {
+            "Content-Type": "text/xml",
+        },
+    })
+        .then((response) => {
+        console.log(response.data);
+        res.json(response.data);
+    })
+        .catch((error) => {
+        console.error("Failed to retrive data", error);
+        res.status(500).send("Failed to fetch data");
+    });
 });
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`);
