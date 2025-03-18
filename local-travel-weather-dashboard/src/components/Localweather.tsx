10d8b0eae1d3566b618f31db9abf50ecc9e911ae
Gustav Liffner <gurra_liffner@hotmail.com>
2025-03-18 12:46:58 +0000

    Update localweather

--- local-travel-weather-dashboard/src/components/Localweather.tsx
+++ local-travel-weather-dashboard/src/components/Localweather.tsx
@@ -1,9 +1,30 @@
 import { useCoordinatesStore } from "../stores/coordinatesStore";
 import { TiWeatherDownpour } from "react-icons/ti";
+import { useState } from "react";
 
 const Localweather = () => {
+  const [Monday, setMonday] = useState({});
+  const [Tuesday, setTuesday] = useState({});
+  const [Wednesday, setWednesday] = useState({});
+  const [Thursday, setThursday] = useState({});
+  const [Friday, setFriday] = useState({});
   const coordinates = useCoordinatesStore((state) => state.coordinates);
   const setCoordinates = useCoordinatesStore((state) => state.setCoordinates);
+
+  const lat = coordinates.lat;
+  const lng = coordinates.lng;
+
+  const fetchWeather = async () => {
+    try {
+      const response = await fetch(
+        `https://api.weatherapi.com/v1/forecast.json?key=4b2f7b4c1b7b4b3e8f1130322210408&q=${lat},${lng}&days=5`,
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <table className="table-auto">
