171fe98177b5f687b8fcb12b4333464b547adfd4
Gustav Liffner <gurra_liffner@hotmail.com>
2025-03-22 22:56:37 +0000

    Localweather completed
    Fetched API. Coordinates added to localweather and inserted in API. Data displaying on table correctly. Styling added to the weather table

--- local-travel-weather-dashboard/src/components/Localweather.tsx
+++ local-travel-weather-dashboard/src/components/Localweather.tsx
@@ -6,19 +6,16 @@ const Localweather = () => {
   const coordinates = useCoordinatesStore((state) => state.coordinates);
   const setCoordinates = useCoordinatesStore((state) => state.setCoordinates);
 
-  // const lat = coordinates.lat;
-  // const lng = coordinates.lng;
-  const lat = 60.45;
-  const lng = -38.67;
+  const lat = coordinates.lat;
+  const lng = coordinates.lng;
   const units = "metric";
-  const cnt = "5";
 
   const apiKey = "2c96b2f76ae370915bbd8a0c18b6789c";
 
   const fetchWeather = async () => {
     try {
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${apiKey}&units=${units}&cnt=${cnt}`,
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${apiKey}&units=${units}`,
       );
       const data = await response.json();
       console.log(data);
@@ -28,57 +25,72 @@ const Localweather = () => {
     }
   };
 
-  useEffect(() => {
-    fetchWeather();
-  }, []);
-
   // useEffect(() => {
-  //   if (lat !== 0 && lng !== 0) {
-  //     fetchWeather();
-  //   }
-  // }, [coordinates]);
+  //   fetchWeather();
+  // }, []);
+
+  useEffect(() => {
+    if (lat !== 0 && lng !== 0) {
+      fetchWeather();
+    }
+  }, [coordinates]);
+
+  const getDayOfWeek = (dateString: string) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString("en-US", { weekday: "long" });
+  };
+
+  const getDailyForecasts = (list: any[]) => {
+    const dailyForecasts: any[] = [];
+    const dates = new Set();
+
+    for (const item of list) {
+      const date = item.dt_txt.split(" ")[0];
+      if (!dates.has(date)) {
+        dates.add(date);
+        dailyForecasts.push(item);
+      }
+      if (dailyForecasts.length === 7) break;
+    }
+
+    return dailyForecasts;
+  };
 
   return (
-    <table className="table-auto">
+    <table className="mx-auto mt-4 w-1/2 table-auto border-2 border-solid border-black">
       <thead>
-        <tr>
+        <tr className="divide-solid divide-black border-2 border-solid border-black">
           <th>Day</th>
-          <th>Temp C</th>
+          <th>Temp °C</th>
           <th>Rain %</th>
           <th>Weather</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Monday</td>
-          <td>{weather?.list?.[0]?.main?.temp ?? "N/A"}</td>
-          <td>{weather?.list?.[0]?.main?.humidity ?? "N/A"}</td>
-          <td>{weather?.list?.[0]?.weather[0]?.icon ?? "N/A"}</td>
-        </tr>
-        <tr>
-          <td>Tuesday</td>
-          <td>{weather?.list?.[1]?.main?.temp ?? "N/A"}</td>
-          <td>{weather?.list?.[0]?.main?.humidity ?? "N/A"}</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Wednesday</td>
-          <td>{weather?.list?.[2]?.main?.temp ?? "N/A"}</td>
-          <td>{weather?.list?.[0]?.main?.humidity ?? "N/A"}</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Thursday</td>
-          <td>{weather?.list?.[3]?.main?.temp ?? "N/A"}</td>
-          <td>{weather?.list?.[0]?.main?.humidity ?? "N/A"}</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>Friday</td>
-          <td>{weather?.list?.[4]?.main?.temp ?? "N/A"}</td>
-          <td>{weather?.list?.[0]?.main?.humidity ?? "N/A"}</td>
-          <td></td>
-        </tr>
+        {weather &&
+          getDailyForecasts(weather.list).map((item: any, index: number) => (
+            <tr
+              key={index}
+              style={{ backgroundColor: index % 2 === 0 ? "#ebebeb" : "white" }}
+            >
+              <td className="border-r-2 border-solid border-black text-center">
+                {getDayOfWeek(item.dt_txt)}
+              </td>
+              <td className="border-r-2 border-solid border-black text-center">
+                {item.main.temp ?? "N/A"}
+              </td>
+              <td className="border-r-2 border-solid border-black text-center">
+                {item.main.humidity ?? "N/A"}
+              </td>
+              <td>
+                <img
+                  src={`http://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
+                  alt="weather icon"
+                  className="mx-auto"
+                />
+              </td>
+            </tr>
+          ))}
       </tbody>
     </table>
   );
