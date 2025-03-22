b08a4aa7b15a75d973c695a4bddda28b53bef67f
Gustav Liffner <gurra_liffner@hotmail.com>
2025-03-22 03:08:30 +0000

    Imported the API
    imported api and also added the weather forecasts to the table

--- local-travel-weather-dashboard/src/components/Localweather.tsx
+++ local-travel-weather-dashboard/src/components/Localweather.tsx
@@ -1,30 +1,42 @@
 import { useCoordinatesStore } from "../stores/coordinatesStore";
-import { TiWeatherDownpour } from "react-icons/ti";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Localweather = () => {
-  const [Monday, setMonday] = useState({});
-  const [Tuesday, setTuesday] = useState({});
-  const [Wednesday, setWednesday] = useState({});
-  const [Thursday, setThursday] = useState({});
-  const [Friday, setFriday] = useState({});
+  const [weather, setWeather] = useState<any>(null);
   const coordinates = useCoordinatesStore((state) => state.coordinates);
   const setCoordinates = useCoordinatesStore((state) => state.setCoordinates);
 
-  const lat = coordinates.lat;
-  const lng = coordinates.lng;
+  // const lat = coordinates.lat;
+  // const lng = coordinates.lng;
+  const lat = 60.45;
+  const lng = -38.67;
+  const units = "metric";
+  const cnt = "5";
+
+  const apiKey = "2c96b2f76ae370915bbd8a0c18b6789c";
 
   const fetchWeather = async () => {
     try {
       const response = await fetch(
-        `https://api.weatherapi.com/v1/forecast.json?key=4b2f7b4c1b7b4b3e8f1130322210408&q=${lat},${lng}&days=5`,
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${apiKey}&units=${units}&cnt=${cnt}`,
       );
       const data = await response.json();
       console.log(data);
+      setWeather(data);
     } catch (error) {
       console.error(error);
     }
   };
+
+  useEffect(() => {
+    fetchWeather();
+  }, []);
+
+  // useEffect(() => {
+  //   if (lat !== 0 && lng !== 0) {
+  //     fetchWeather();
+  //   }
+  // }, [coordinates]);
 
   return (
     <table className="table-auto">
@@ -39,43 +51,33 @@ const Localweather = () => {
       <tbody>
         <tr>
           <td>Monday</td>
-          <td>Malcolm Lockyer</td>
-          <td>1961</td>
-          <td>
-            <TiWeatherDownpour />
-          </td>
+          <td>{weather?.list?.[0]?.main?.temp ?? "N/A"}</td>
+          <td>{weather?.list?.[0]?.main?.humidity ?? "N/A"}</td>
+          <td>{weather?.list?.[0]?.weather[0]?.icon ?? "N/A"}</td>
         </tr>
         <tr>
           <td>Tuesday</td>
-          <td>The Eagles</td>
-          <td>1972</td>
-          <td>
-            <TiWeatherDownpour />
-          </td>
+          <td>{weather?.list?.[1]?.main?.temp ?? "N/A"}</td>
+          <td>{weather?.list?.[0]?.main?.humidity ?? "N/A"}</td>
+          <td></td>
         </tr>
         <tr>
           <td>Wednesday</td>
-          <td>Earth, Wind, and Fire</td>
-          <td>1975</td>
-          <td>
-            <TiWeatherDownpour />
-          </td>
+          <td>{weather?.list?.[2]?.main?.temp ?? "N/A"}</td>
+          <td>{weather?.list?.[0]?.main?.humidity ?? "N/A"}</td>
+          <td></td>
         </tr>
         <tr>
           <td>Thursday</td>
-          <td>Earth, Wind, and Fire</td>
-          <td>1975</td>
-          <td>
-            <TiWeatherDownpour />
-          </td>
+          <td>{weather?.list?.[3]?.main?.temp ?? "N/A"}</td>
+          <td>{weather?.list?.[0]?.main?.humidity ?? "N/A"}</td>
+          <td></td>
         </tr>
         <tr>
           <td>Friday</td>
-          <td>Earth, Wind, and Fire</td>
-          <td>1975</td>
-          <td>
-            <TiWeatherDownpour />
-          </td>
+          <td>{weather?.list?.[4]?.main?.temp ?? "N/A"}</td>
+          <td>{weather?.list?.[0]?.main?.humidity ?? "N/A"}</td>
+          <td></td>
         </tr>
       </tbody>
     </table>
