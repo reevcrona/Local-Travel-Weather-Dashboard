8788de8123c729c435a1572b5d44133f7a3d63f0
reevcrona <89569507+reevcrona@users.noreply.github.com>
2025-04-02 17:49:33 +0200

    adjusted the filter buttons styling depening on if it is seleceted or not

--- local-travel-weather-dashboard/src/components/TrafficFilterButton.tsx
+++ local-travel-weather-dashboard/src/components/TrafficFilterButton.tsx
@@ -8,7 +8,7 @@ function TrafficFilterButton({
 }: trafficFilterButtonProps) {
   return (
     <button
-      className={`cursor-pointer rounded-lg border-2 ${activeFilter === filtervalue.eng ? "border-white" : "border-[#4b5563]"} bg-transparent p-2 font-bold text-white hover:border-[#9ca3af]`}
+      className={`cursor-pointer rounded-lg border-2 transition-colors duration-200 ease-in-out ${activeFilter === filtervalue.eng ? "border-transparent bg-white text-black" : "border-[#4b5563] bg-transparent text-white hover:border-[#9ca3af]"} p-2 font-bold`}
       onClick={() => handleFilterChange(filtervalue.eng)}
     >
       {filtervalue.swe} {trafficAmount}
