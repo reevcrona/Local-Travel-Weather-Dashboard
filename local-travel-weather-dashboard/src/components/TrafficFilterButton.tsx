58912c10cf107e9351346b5fc4f8daa4447f4a75
Jreev <jacob.reevcrona@yahoo.com>
2025-04-06 21:32:14 +0200

    changed filter buttons font size

--- local-travel-weather-dashboard/src/components/TrafficFilterButton.tsx
+++ local-travel-weather-dashboard/src/components/TrafficFilterButton.tsx
@@ -8,7 +8,7 @@ function TrafficFilterButton({
 }: trafficFilterButtonProps) {
   return (
     <button
-      className={`cursor-pointer rounded-lg border-2 text-[8px] transition-colors duration-200 ease-in-out min-[385px]:text-xs ${activeFilter === filtervalue.eng ? "border-transparent bg-white text-black" : "border-[#4b5563] bg-transparent text-white hover:border-[#9ca3af]"} p-3 font-bold`}
+      className={`cursor-pointer rounded-lg border-2 text-[8px] transition-colors duration-200 ease-in-out min-[385px]:text-base ${activeFilter === filtervalue.eng ? "border-transparent bg-white text-black" : "border-[#4b5563] bg-transparent text-white hover:border-[#9ca3af]"} p-3 font-bold`}
       onClick={() => handleFilterChange(filtervalue.eng)}
     >
       {filtervalue.swe} {trafficAmount}
